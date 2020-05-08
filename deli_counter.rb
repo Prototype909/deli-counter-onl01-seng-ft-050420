c71737d1c6e6f449bc819a1e5cb51e2bb6be7733
Steve Anderson <prototype909@msn.com>
2020-05-08 01:05:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line(people)
   if people.length == 0
     puts "The line is currently empty."
   else
-    numbered_people = []
+    numbered_people = [katz_deli]
     people.each do |i|
       numbered_people.push " #{people.index(i) + 1}. #{i}"
     end
